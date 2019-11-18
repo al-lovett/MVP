61170f8da5c7d160cb2b0480a3d2faf55bb952a3
Al Lovett <alovett1215@gmail.com>
2019-11-18 16:28:26 -0700

    testing customer form entry

---
+++ resources/views/services.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Add A Customer</title>
+  </head>
+  <body>
+
+    <div class="app">
+      <form>
+        <ul>
+          <li>1-Name</li>
+          <li>2-Service Type</li>
+          <li>3-Description</li>
+          <li>4-Total</li>
+          <li>5-Client</li>
+          <li>6-Location</li>
+          <li>7-Job Number</li>
+          <li>8-Payment Method</li>
+        </ul>
+      </form>
+    </div>
+  </body>
+</html>
